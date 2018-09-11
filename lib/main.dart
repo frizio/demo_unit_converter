136d1d1bd286a160aad9a4a95c6bf62e2caf67d6
Maurizio La Rocca <mrz.larocca@gmail.com>
2018-09-11 14:44:54 +0200

    03 - Add CategoryRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demo_unit_converter/category.dart';
+import 'package:demo_unit_converter/category_routee.dart';
 import 'package:flutter/material.dart';
 
 const _categoryName = 'Cake';
@@ -13,28 +13,8 @@ class UnitConverterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Unit Converter',
-      home: Scaffold(
-        backgroundColor: Colors.green[100],
-        appBar: AppBar(
-            elevation: 0.0,
-            title: Text(
-              'The Unit Converter',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 30.0,
-              ),
-            ),
-            centerTitle: true),
-        body: Center(
-          child: Category(
-            name: _categoryName,
-            color: _categoryColor,
-            iconLocation: _categoryIcon,
-          ),
-        ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Unit Converter',
+        home: CategoryRoute());
   }
 }
